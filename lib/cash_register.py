ea58534f27ccc67ca743a02810f0dd71f42ce53e
samson kangara <samkangara06@gmail.com>
2026-06-10 08:58:20 +0300

    feat:add .

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,54 @@
-#!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self._discount = 0
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.previous_transactions = []
+
+    @property
+    def discount(self):
+        return self._discount
+
+    @discount.setter
+    def discount(self, value):
+        if isinstance(value, int) and 0 <= value <= 100:
+            self._discount = value
+        else:
+            print("Not valid discount")
+
+    def add_item(self, item, price, quantity):
+        self.total += price * quantity
+
+        for _ in range(quantity):
+            self.items.append(item)
+
+        self.previous_transactions.append({
+            "item": item,
+            "price": price,
+            "quantity": quantity
+        })
+
+    def apply_discount(self):
+        if not self.previous_transactions:
+            print("There is no discount to apply.")
+            return
+
+        discount_amount = self.total * (self.discount / 100)
+        self.total -= discount_amount
+
+    def void_last_transaction(self):
+        if not self.previous_transactions:
+            return
+
+        last_transaction = self.previous_transactions.pop()
+
+        item = last_transaction["item"]
+        price = last_transaction["price"]
+        quantity = last_transaction["quantity"]
+
+        self.total -= price * quantity
+
+        for _ in range(quantity):
+            if item in self.items:
+                self.items.remove(item)
